3cad160684e063ba43ecff4dcd9138dbf77115de
isims <isims@92d09ebd-acfe-0310-b288-9e69a966d206>
2011-01-07 16:44:35 +0000

    using the newly added FormattedConsole class for output
    git-svn-id: https://www.spree.de/svn-repository/ApacheBSIAuditHelper/trunk@53197 92d09ebd-acfe-0310-b288-9e69a966d206

--- HttpdConfigParser/src/org/akquinet/audit/bsi/httpd/Frage5a.java
+++ HttpdConfigParser/src/org/akquinet/audit/bsi/httpd/Frage5a.java
@@ -2,18 +2,22 @@ package org.akquinet.audit.bsi.httpd;
 
 import java.util.List;
 
+import org.akquinet.audit.FormattedConsole;
 import org.akquinet.audit.YesNoQuestion;
+import org.akquinet.audit.FormattedConsole.OutputLevel;
 import org.akquinet.httpd.ConfigFile;
 import org.akquinet.httpd.syntax.Directive;
 
 public class Frage5a extends YesNoQuestion
 {
 	private ConfigFile _conf;
+	private FormattedConsole _console;
+	private final FormattedConsole.OutputLevel _level = FormattedConsole.OutputLevel.Q2;
 	
-	public Frage5a(ConfigFile conf)
+	public Frage5a(ConfigFile conf, FormattedConsole console)
 	{
-		// TODO Auto-generated method stub
 		_conf = conf;
+		_console = console;
 	}
 
 	/**
@@ -26,10 +30,16 @@ public class Frage5a extends YesNoQuestion
 		
 		if(!incList.isEmpty())
 		{
+			_console.printAnswer(_level, false, "There are Include-directives in your apache configuration:");
+			for (Directive directive : incList)
+			{
+				_console.println(_level, directive.getLinenumber() + ": " + directive.getName() + " " + directive.getValue());
+			}
 			return false;
 		}
 		else
 		{
+			_console.printAnswer(OutputLevel.Q2, true, "");
 			return true;
 		}
 	}
