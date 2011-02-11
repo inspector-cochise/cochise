28829e56c7770620910609d8e750ad0ad546c2d2
isims <isims@92d09ebd-acfe-0310-b288-9e69a966d206>
2011-02-11 16:15:05 +0000

    first try on question 9 related stuff
    git-svn-id: https://www.spree.de/svn-repository/ApacheBSIAuditHelper/trunk@55022 92d09ebd-acfe-0310-b288-9e69a966d206

---
+++ ScriptedChecker/quest9.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# $1 should be the ServerRoot directory
+# $2 should be the user under which the apache is started
+
+#check permissions are set to something like ***------ and owning user is $2
+if stat --printf="%n %A %U" `find -L $1` | grep -v $1/htdocs | awk -v USER=$2 -f q9.awk
+then
+		#echo -e "OK"
+		exit 0
+fi
+
+#echo -e "NOTOK"
+exit 1
