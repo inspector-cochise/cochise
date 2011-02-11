eefed56d9f5089da42868bb39513e7215f515b33
isims <isims@92d09ebd-acfe-0310-b288-9e69a966d206>
2011-02-11 11:08:29 +0000

    little bugfix, now there can be more than one file in $1 which contains $2 in its name now we scan wordwise so only exact machtes matter and if there are other files (with maybe different permissions) there will be no problem
    git-svn-id: https://www.spree.de/svn-repository/ApacheBSIAuditHelper/trunk@54979 92d09ebd-acfe-0310-b288-9e69a966d206

--- ScriptedChecker/quest8.sh
+++ ScriptedChecker/quest8.sh
@@ -3,10 +3,10 @@
 # $2 should be the config file (filename only, no path)
 
 #check owning group is root and permissions are set to something like ******---
-if ls -l $1 | grep  $2 | awk -f q8.awk
+if ls -l $1 | grep -w $2 | awk -f q8.awk
 then
 	#now check the owning user is a member of the group root
-	owningUser=`ls -l $1 | grep $2 | awk '{print $3}'`
+	owningUser=`ls -l $1 | grep -w $2 | awk '{print $3}'`
 	if cat /etc/group | grep ^root | awk 'BEGIN {FS=":"} {print $4;}' | awk 'BEGIN {RS=","} {print $0;}' | grep $owningUser >& /dev/null
 	then
 		echo -e "OK   \t\tYour main configuration file seems to be safe."
