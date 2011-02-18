3dd1de774ee1fc1226dc8cf34073419e7594fa35
isims <isims@92d09ebd-acfe-0310-b288-9e69a966d206>
2011-02-18 14:57:17 +0000

    Version featuring all questions from 1 to 11!
    git-svn-id: https://www.spree.de/svn-repository/ApacheBSIAuditHelper/trunk@55385 92d09ebd-acfe-0310-b288-9e69a966d206

--- HttpdConfigParser/src/org/akquinet/audit/bsi/httpd/HttpdAudit.java
+++ HttpdConfigParser/src/org/akquinet/audit/bsi/httpd/HttpdAudit.java
@@ -14,8 +14,10 @@ import org.akquinet.audit.bsi.httpd.software.Quest4;
 import org.akquinet.audit.bsi.httpd.software.Quest5;
 import org.akquinet.audit.bsi.httpd.software.Quest6;
 import org.akquinet.audit.bsi.httpd.software.Quest7;
+import org.akquinet.audit.bsi.httpd.usersNrights.Quest10;
 import org.akquinet.audit.bsi.httpd.usersNrights.Quest11;
 import org.akquinet.audit.bsi.httpd.usersNrights.Quest8;
+import org.akquinet.audit.bsi.httpd.usersNrights.Quest9;
 import org.akquinet.httpd.ConfigFile;
 
 public class HttpdAudit
@@ -30,13 +32,15 @@ public class HttpdAudit
 		//for now this is just for debugging
 		try
 		{
+			File configFile = null;
 			ConfigFile conf = null;
 			File apacheExecutable = null;
 			boolean highSec = true;	//TODO initialize me in some way
 			switch(args.length)
 			{
 			case 2:
-				conf = new ConfigFile(new File(args[0]));
+				configFile = new File(args[0]);
+				conf = new ConfigFile(configFile);
 				apacheExecutable = new File(args[1]);
 				break;
 			default:
@@ -53,7 +57,9 @@ public class HttpdAudit
 			tmpList.add(new Quest5(conf));
 			tmpList.add(new Quest6(apacheExecutable));
 			tmpList.add(new Quest7(conf));
-			tmpList.add(new Quest8(apacheExecutable));
+			tmpList.add(new Quest8(configFile));
+			tmpList.add(new Quest9(conf));
+			tmpList.add(new Quest10(conf));
 			tmpList.add(new Quest11(conf));
 			
 			InteractiveAsker asker = new InteractiveAsker(tmpList);
