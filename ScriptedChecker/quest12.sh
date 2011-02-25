8ce736f25927f55db81cefce155f358612f494ce
isims <isims@92d09ebd-acfe-0310-b288-9e69a966d206>
2011-02-25 10:40:14 +0000

    big changes so we don't necessarily need the second parameter anymore
    git-svn-id: https://www.spree.de/svn-repository/ApacheBSIAuditHelper/trunk@55680 92d09ebd-acfe-0310-b288-9e69a966d206

--- ScriptedChecker/quest12.sh
+++ ScriptedChecker/quest12.sh
@@ -1,49 +1,60 @@
 #!/bin/bash
 # $1 should be the user
-# $2 should be the group
+# $2 (optional) should be the group
 
 #does $1 exist?
 if passwd -S $1 >& /dev/null
 then
+	false
+else
 	echo User $1 does not exist.
 	exit 1
 fi
 
+if test $# -gt 1
+then
+	groupName=$2
+	group=`cat /etc/group | grep -w ^$groupName | awk 'BEGIN {FS=":"} {print $3;}'`
+else
+	group=`cat /etc/passwd |  grep -w ^$1 | awk 'BEGIN {FS=":"} {print $4;}'`
+	groupName=`cat /etc/group | grep :$group: | awk 'BEGIN {FS=":"} {print $1;}'`
+fi
+
 #is $1 locked?
-if passwd -S $1 | awk '{if $2 ~ /L/ {exit 1} else {exit 0} }'
+if passwd -S $1 | awk '{if($2 ~ /L/) {exit 1} else {exit 0} }'
 then
 	echo User $1 is not locked. A login would may be possible.
 	exit 1
 fi
 
-#first check $1 is the only member of $2
-nmbrOfMembers=`cat /etc/group | grep ^$2 | awk 'BEGIN {FS=":"} {print $4;}' | awk 'BEGIN {RS=","} {print $0;}' | wc -w`
+#first check $1 is the only member of $group
+nmbrOfMembers=`cat /etc/group | grep :$group: | awk 'BEGIN {FS=":"} {print $4;}' | awk 'BEGIN {RS=","} {print $0;}' | wc -w`
 
-#is $1 a member of $2?
-if cat /etc/group | grep ^$2 | awk 'BEGIN {FS=":"} {print $4;}' | awk 'BEGIN {RS=","} {print $0;}' | grep $1 >& /dev/null
+#is $1 a member of $group?
+if cat /etc/group | grep :$group: | awk 'BEGIN {FS=":"} {print $4;}' | awk 'BEGIN {RS=","} {print $0;}' | grep $1 >& /dev/null
 then
-	#is $1 the only member of $2
+	#is $1 the only member of $group
 	if test $nmbrOfMembers -ne 1
 	then
-		echo $1 is not the only member of $2.
+		echo $1 is not the only member of $groupName.
 		exit 1
 	fi
 	
 	#is $1 member of any other group?
-	if cat /etc/group | grep -v ^$2 | awk 'BEGIN {FS=":"} {print $4;}' | awk 'BEGIN {RS=","} {print $0;}' | grep $1
+	if cat /etc/group | grep -v :$group: | awk 'BEGIN {FS=":"} {print $4;}' | awk 'BEGIN {RS=","} {print $0;}' | grep $1 >& /dev/null
 	then
-		echo $1 is member of another group than only $2.
+		echo $1 is member of another group than only $groupName.
 		exit 1
 	fi
 	
-	echo Seems like $1 is the only member of $2 and not member of any other group.
+	echo Seems like $1 is the only member of $groupName and not member of any other group.
 	
-#is there any member in $2
+#is there any member in $group
 elif test $nmbrOfMembers -eq 0
 then
-	echo $2 is empty and $1 is not a member of any other group.
+	echo $groupName is empty and $1 is not a member of any other group.
 else
-	echo $1 is not member of $2 but there are other members in $2.
+	echo $1 is not member of $groupName but there are other members in $groupName.
 	exit 1
 fi
 
