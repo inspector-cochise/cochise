f24943d2ab5b8307f71625a06bb297eace96b12d
isims <isims@92d09ebd-acfe-0310-b288-9e69a966d206>
2011-02-25 14:34:46 +0000

    first files
    git-svn-id: https://www.spree.de/svn-repository/ApacheBSIAuditHelper/trunk@55694 92d09ebd-acfe-0310-b288-9e69a966d206

---
+++ ApacheBSIAuditHelpterUnitTests/src/org/akquinet/audit/bsi/httpd/software/Quest3Test.java
@@ -0,0 +1,62 @@
+package org.akquinet.audit.bsi.httpd.software;
+
+import java.io.File;
+import java.io.IOException;
+import static org.junit.Assert.*;
+import org.akquinet.httpd.ConfigFile;
+import org.junit.Test;
+
+public class Quest3Test
+{
+	File _senselessExecutable = new File("C:\\autoexec.bat");
+	
+	File _negativeTrivial = new File("./testFiles/Quest3/neg_triv.conf"); 
+	File _negativeSimilar = new File("./testFiles/Quest3/neg_sim.conf"); 
+	File _negativeMalformed = new File("./testFiles/Quest3/neg_malf.conf"); 
+	File _negativeConditionally = new File("./testFiles/Quest3/neg_cond.conf"); 
+	
+	File _positiveTrivial = new File("./testFiles/Quest3/pos_triv.conf"); 
+	File _positiveMalformed = new File("./testFiles/Quest3/pos_malf.conf"); 
+
+	@Test
+	public final void testNegativeTrivial() throws IOException
+	{
+		Quest3 SUT = new Quest3(new ConfigFile(_negativeTrivial), _senselessExecutable);
+		assertFalse(SUT.answer());
+	}
+	
+	@Test
+	public final void testNegativeSimilar() throws IOException
+	{
+		Quest3 SUT = new Quest3(new ConfigFile(_negativeSimilar), _senselessExecutable);
+		assertFalse(SUT.answer());
+	}
+	
+	@Test
+	public final void testNegativeMalformed() throws IOException
+	{
+		Quest3 SUT = new Quest3(new ConfigFile(_negativeMalformed), _senselessExecutable);
+		assertFalse(SUT.answer());
+	}
+	
+	@Test
+	public final void testNegativeConditionally() throws IOException
+	{
+		Quest3 SUT = new Quest3(new ConfigFile(_negativeConditionally), _senselessExecutable);
+		assertFalse(SUT.answer());
+	}
+	
+	@Test
+	public final void testPositiveTrivial() throws IOException
+	{
+		Quest3 SUT = new Quest3(new ConfigFile(_positiveTrivial), _senselessExecutable);
+		assertTrue(SUT.answer());
+	}
+	
+	@Test
+	public final void testPositiveMalformed() throws IOException
+	{
+		Quest3 SUT = new Quest3(new ConfigFile(_positiveMalformed), _senselessExecutable);
+		assertTrue(SUT.answer());
+	}
+}
