c4e96ddb2d3110ad5033d5f02c56f7c3c0aaaebd
isims <isims@92d09ebd-acfe-0310-b288-9e69a966d206>
2011-03-04 09:57:29 +0000

    checking in positive trivial and positive malformed
    git-svn-id: https://www.spree.de/svn-repository/ApacheBSIAuditHelper/trunk@56058 92d09ebd-acfe-0310-b288-9e69a966d206

---
+++ ApacheBSIAuditHelpterUnitTests/testFiles/Quest3/pos_malf.bat
@@ -0,0 +1,5 @@
+echo   core.c 1>&2
+echo Compiled in modules: 1>&2
+echo   m..od_logio.c 1>&2
+echo
+echo   mod_security.c 1>&2
---
+++ ApacheBSIAuditHelpterUnitTests/testFiles/Quest3/pos_triv.bat
@@ -0,0 +1,6 @@
+echo Compiled in modules: 1>&2
+echo   core.c 1>&2
+echo   mod_log_config.c 1>&2
+echo   mod_logio.c 1>&2
+echo   mod_so.c 1>&2
+echo   mod_security.c 1>&2
