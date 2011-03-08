6eb1cd6818e20a6e8f6cfdf8ec2a42dc9d63c00b
isims <isims@92d09ebd-acfe-0310-b288-9e69a966d206>
2011-03-08 16:39:05 +0000

    fixing problem with no or multiple ServerRoot directives
    git-svn-id: https://www.spree.de/svn-repository/ApacheBSIAuditHelper/trunk@56271 92d09ebd-acfe-0310-b288-9e69a966d206

--- HttpdConfigParser/src/org/akquinet/audit/bsi/httpd/usersNrights/Quest9.java
+++ HttpdConfigParser/src/org/akquinet/audit/bsi/httpd/usersNrights/Quest9.java
@@ -30,27 +30,39 @@ public class Quest9 implements YesNoQuestion
 		_console.println(FormattedConsole.OutputLevel.HEADING, "vvvv" + _id + "vvvv");
 
 		_console.printAnswer(_level, null, "We'll now start to examine the permissions in your ServerRoot.");
-		String user = _console.askStringQuestion(_level, "Which user starts the apache web server? [root]");
-		if(user.equals(""))
+		if(_serverRoot != null)
 		{
-			user = "root";
+			String user = _console.askStringQuestion(_level, "Which user starts the apache web server? [root]");
+			if(user.equals(""))
+			{
+				user = "root";
+			}
+			
+			_q9a = new Quest9a(_serverRoot, user);
+			_q9b = new Quest9b(_serverRoot);
+			
+			boolean ret = _q9a.answer();
+			ret &= _q9b.answer();
+			_console.println(FormattedConsole.OutputLevel.HEADING, "^^^^" + _id + "^^^^");
+			_console.printAnswer(_level, ret, ret ? "Your ServerRoot seems ok." : "Seems like your ServerRoot is unsafe in the way mentioned above.");
+			return ret;
 		}
-		
-		_q9a = new Quest9a(_serverRoot, user);
-		_q9b = new Quest9b(_serverRoot);
-		
-		boolean ret = _q9a.answer();
-		ret &= _q9b.answer();
-		_console.println(FormattedConsole.OutputLevel.HEADING, "^^^^" + _id + "^^^^");
-		_console.printAnswer(_level, ret, ret ? "Your ServerRoot seems ok." : "Seems like your ServerRoot is unsafe in the way mentioned above.");
-		
-		return ret;
+		else
+		{
+			_console.println(FormattedConsole.OutputLevel.HEADING, "^^^^" + _id + "^^^^");
+			_console.printAnswer(_level, false, "Either none or multiple ServerRoot directives found. There has to be exactly one.");
+			return false;
+		}
 	}
 
 	private String getServerRoot()
 	{
-		//TODO handle problems like multiple ServerRoot directives or ServerRoot not in global context
 		List<Directive> srvRootList = _conf.getDirective("ServerRoot");
+		if(srvRootList.size() != 1)
+		{
+			return null;
+		}
+		
 		String ret = srvRootList.get(0).getValue().trim();
 		if(ret.startsWith("\"") && ret.endsWith("\"") && ret.length() > 1)
 		{
