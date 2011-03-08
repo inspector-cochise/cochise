0d829c5fc0b335f9665395a6a8741d586980676d
isims <isims@92d09ebd-acfe-0310-b288-9e69a966d206>
2011-03-08 15:40:24 +0000

    more flexibility with the script and it's location
    git-svn-id: https://www.spree.de/svn-repository/ApacheBSIAuditHelper/trunk@56247 92d09ebd-acfe-0310-b288-9e69a966d206

--- HttpdConfigParser/src/org/akquinet/audit/bsi/httpd/usersNrights/Quest8.java
+++ HttpdConfigParser/src/org/akquinet/audit/bsi/httpd/usersNrights/Quest8.java
@@ -11,19 +11,27 @@ public class Quest8 implements YesNoQuestion
 	private static final String _id = "Quest8";
 	private static final FormattedConsole _console = FormattedConsole.getDefault();
 	private static final FormattedConsole.OutputLevel _level = FormattedConsole.OutputLevel.Q1;
-	private static final String _command = "./QfileSafe.sh";
+	private String _command;
+	private String _commandPath;
 	private File _configFile;
 
 	public Quest8(File configFile)
 	{
+		this(configFile, "./", "QfileSafe.sh");
+	}
+
+	public Quest8(File configFile, String commandPath, String command)
+	{
 		_configFile = configFile;
+		_commandPath = commandPath;
+		_command = command;
 	}
 
 	@Override
 	public boolean answer()
 	{
 		_console.println(FormattedConsole.OutputLevel.HEADING, "----" + _id + "----");
-		ShellAnsweredQuestion quest = new ShellAnsweredQuestion(_command, _configFile.getAbsolutePath());
+		ShellAnsweredQuestion quest = new ShellAnsweredQuestion(_commandPath + _command, _configFile.getAbsolutePath());
 		boolean ret = quest.answer();
 		
 		_console.printAnswer(_level, ret, ret ? "Your main configuration file seems to be safe."
