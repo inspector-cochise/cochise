83f105f5c7023d88ba3781b4c953b1e89408a9b5
cochise <immanuel.sims@akquinet.de>
2011-04-15 11:20:17 +0200

    first showcase for the hiding paragraphs (all the text should be somehow externalized)

--- ApacheBSIAuditHelper/src/org/akquinet/audit/bsi/httpd/os/Quest1.java
+++ ApacheBSIAuditHelper/src/org/akquinet/audit/bsi/httpd/os/Quest1.java
@@ -36,6 +36,18 @@ public class Quest1 implements YesNoQuestion
 			_uc.printAnswer(ret, ret ? "User root is correctly locked."
 								: "Please lock user root (\"passwd -l\"). After that you can't directly log yourself in as root.");
 			
+			_uc.beginHidingParagraph("(more Information)");
+			_uc.printParagraph("For a high level of security you should block access to the standard administrative accounts. " +
+					"This adds security to your server because an attacker in many cases also needs to know the name of an administrative account.");
+			_uc.printParagraph("On most Linux systems you can lock the root account by executing the following command (USE WITH CARE!)");
+			_uc.printExample("passwd -l root");
+			_uc.printParagraph("Please ensure that you first created another account with administrative rights. " +
+					"Because you won't be able to login as root anymore after that. " +
+					"(But you can still use sudo.) " +
+					"If you ever need to unlock your root account you can do so by executing the following command.");
+			_uc.printExample("passwd -u root");
+			_uc.endHidingParagraph();
+			
 			return ret;
 		}
 		else
