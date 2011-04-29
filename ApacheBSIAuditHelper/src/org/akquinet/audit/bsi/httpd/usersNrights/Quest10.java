1c3417fbbb0bd2e413673a450a29335a7cb377e6
cochise <immanuel.sims@akquinet.de>
2011-04-29 13:57:36 +0200

    prevent full shutdown at bad syntax

--- ApacheBSIAuditHelper/src/org/akquinet/audit/bsi/httpd/usersNrights/Quest10.java
+++ ApacheBSIAuditHelper/src/org/akquinet/audit/bsi/httpd/usersNrights/Quest10.java
@@ -87,7 +87,15 @@ public class Quest10 extends ModuleHelper implements YesNoQuestion
 		List<Directive> dirList = getLoadModuleList();
 		for (Directive dir : dirList)
 		{
-			ret.add(new File(dir.getValue().trim().split("[ \t]+")[1]));
+			try
+			{
+				ret.add(new File(dir.getValue().trim().split("[ \t]+")[1]));
+			}
+			catch(IndexOutOfBoundsException e)
+			{
+				//This is a malformed LoadModule directive. Actually "apache2 -t" recognises this as error.
+				//So it's ok not to do anything here 'cause it will be handled in Quest6.
+			}
 		}
 		
 		dirList = getLoadFileList();
