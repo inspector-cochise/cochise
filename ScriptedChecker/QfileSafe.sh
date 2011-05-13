07ef1a251fabbfff4f1f1482f187da969303f96f
inspector-cochise <immanuel.sims@akquinet.de>
2011-05-13 15:00:28 +0200

    only check for owning_user==root

--- ScriptedChecker/QfileSafe.sh
+++ ScriptedChecker/QfileSafe.sh
@@ -7,19 +7,11 @@ then
 	pat=$2
 fi
 
-#check owning group is root and permissions are set to something like ******---
-if stat -L -c "%A %G" $1 | awk -v PATTERN=$pat -f QfileSafe.awk
+if stat -L -c "%A %U" $1 | awk -v PATTERN=$pat -f QfileSafe.awk
 then
-	#now check the owning user is a member of the group root
-	owningUser=`stat -L -c "%U" $1`
-	
-	#is $owningUser a member of root?
-	if cat /etc/group | grep ^root | awk 'BEGIN {FS=":"} {print $4;}' | awk 'BEGIN {RS=","} {print $0;}' | grep $owningUser >& /dev/null
-	then
-		echo -e "OK   \t\tYour main file seems to be safe."
-		exit 0
-	fi
+	echo -e "OK   \t\tYour file seems to be safe."
+	exit 0
 fi
 
-echo -e "NOTOK\t\tUsers not in the group \"root\" may have some access to your file."
+echo -e "NOTOK\t\tOther Users than \"root\" may have some access to your file."
 exit 1
