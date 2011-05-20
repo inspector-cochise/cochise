ee5826eede7ad0d34a96859c320d1f79ab3574bb
inspector-cochise <immanuel.sims@akquinet.de>
2011-05-20 11:26:39 +0200

    this should be a hidingParagraph

--- ApacheBSIAuditHelper/src/org/akquinet/audit/bsi/httpd/usersNrights/Quest12.java
+++ ApacheBSIAuditHelper/src/org/akquinet/audit/bsi/httpd/usersNrights/Quest12.java
@@ -37,7 +37,7 @@ public class Quest12 implements YesNoQuestion
 		_uc.printParagraph( _labels.getString("Q0") );
 		
 		_uc.println( _labels.getString("L1") );
-		_uc.printParagraph( _labels.getString("P1") );
+		_uc.printHidingParagraph( _labels.getString("S0"), _labels.getString("P1") );
 		
 		_uc.beginIndent();
 			boolean ret = _q12b.answer();
