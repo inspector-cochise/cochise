be36b5c6111a00ced5d148f061a025c3c9b00c12
inspector-cochise <immanuel.sims@akquinet.de>
2011-11-18 19:08:49 +0100

    Implemented remote version check (see #11). Test has still to be updated.

--- ApacheBSIAuditHelper/src/org/akquinet/audit/bsi/httpd/software/Quest2.java
+++ ApacheBSIAuditHelper/src/org/akquinet/audit/bsi/httpd/software/Quest2.java
@@ -3,6 +3,7 @@ package org.akquinet.audit.bsi.httpd.software;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.text.MessageFormat;
 import java.util.ResourceBundle;
 
 import org.akquinet.audit.YesNoQuestion;
@@ -13,14 +14,23 @@ public class Quest2 implements YesNoQuestion
 	private static final String _id = "Quest2";
 	private static final UserCommunicator _uc = UserCommunicator.getDefault();
 	private ProcessBuilder _httpd;
+	private ProcessBuilder _getCurrent;
+	private ProcessBuilder _getRunning;
 	private ResourceBundle _labels;
 	
 	public Quest2(File apacheExecutable)
+	{
+		this(apacheExecutable, "./", "newestVersion.sh", "runningVersion.sh");
+	}
+	
+	public Quest2(File apacheExecutable, String commandPath, String getCurrentVersionCommand, String getRunningVersionCommand)
 	{
 		_labels = ResourceBundle.getBundle(_id, _uc.getLocale());
 		try
 		{
 			_httpd = new ProcessBuilder(apacheExecutable.getCanonicalPath(), "-v");
+			_getCurrent = new ProcessBuilder(commandPath + getCurrentVersionCommand);
+			_getRunning = new ProcessBuilder(commandPath + getRunningVersionCommand, apacheExecutable.getAbsolutePath());
 		}
 		catch (IOException e)
 		{
@@ -34,13 +44,107 @@ public class Quest2 implements YesNoQuestion
 		_uc.printHeading3(_id);
 		_uc.printParagraph( _labels.getString("Q0") );
 		
-		Process p;
-		String version = _labels.getString("S1");
+		String version = getRunningVersionInformation();
+		
+		_uc.println(_labels.getString("S2"));
+		_uc.printExample(version);
+		
+		if(isNewest())
+		{
+			_uc.printAnswer(true, _labels.getString("S4"));
+			return true;
+		}
+		else
+		{
+			String newer = getNewestStableVersion();
+			_uc.printParagraph( MessageFormat.format(_labels.getString("S5"), newer) );
+			
+			boolean ret = _uc.askYesNoQuestion(_labels.getString("Q1"));
+			_uc.printAnswer(ret, ret ? _labels.getString("S3_good")
+					: _labels.getString("S3_bad"));
+			return ret;
+		}
+	}
+
+	private boolean isNewest()
+	{
+		String running = getRunningVersion();
+		String newest = getNewestStableVersion();
+		
+		String[] tmp;
+		tmp = running.split("\\.");
+		int running_Major = Integer.parseInt(tmp[0]);
+		int running_Minor = Integer.parseInt(tmp[1]);
+		int running_release = Integer.parseInt(tmp[2]);
+		
+		tmp = newest.split("\\.");
+		int newest_Major = Integer.parseInt(tmp[0]);
+		int newest_Minor = Integer.parseInt(tmp[1]);
+		int newest_release = Integer.parseInt(tmp[2]);
+		
+		if(running_Major > newest_Major ||
+		   running_Major == newest_Major && running_Minor > newest_Minor ||
+		   running_Major == newest_Major && running_Minor == newest_Minor && running_release >= newest_release)
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+
+	private String getRunningVersion()
+	{
+		String version = getProcessOutput(_getRunning);
+		if(version.equals(""))
+		{
+			throw new RuntimeException( _labels.getString("S1") );
+		}
+		return version.substring(0, version.length()-1);
+	}
+
+	private String getNewestStableVersion()
+	{
+		String version = getProcessOutput(_getCurrent);
+		if(version.equals(""))
+		{
+			throw new RuntimeException( _labels.getString("S1") );
+		}
+		return version.substring(0, version.length()-1);
+	}
+
+	private String getRunningVersionInformation()
+	{
+		String version = getProcessOutput(_httpd);
+		if(version.equals(""))
+		{
+			throw new RuntimeException( _labels.getString("S1") );
+		}
+		return version.substring(0, version.length()-1);
+	}
+
+	private String getProcessOutput(ProcessBuilder pb)
+	{
+		String output = "";
 		try
 		{
-			p = _httpd.start();
+			Process p = pb.start();
 			InputStream stdOut = p.getInputStream();
-			p.waitFor();
+			boolean wait = true;
+
+			while(wait)
+			{
+				try
+				{
+					p.waitFor();
+					wait = false;
+				}
+				catch (InterruptedException e)
+				{
+					Thread.currentThread().interrupt();
+				}
+			}
 			
 			StringBuffer buf = new StringBuffer();
 			int b = stdOut.read();
@@ -49,24 +153,15 @@ public class Quest2 implements YesNoQuestion
 				buf.append((char)b);
 				b = stdOut.read();
 			}
-			version = buf.toString();
+			output = buf.toString();
 		}
 		catch (IOException e)
 		{
 			throw new RuntimeException(e);
 		}
-		catch (InterruptedException e)
-		{
-			throw new RuntimeException(e);
-		}
-		_uc.println(_labels.getString("S2"));
-		_uc.printExample(version);
-		boolean ret = _uc.askYesNoQuestion(_labels.getString("Q1"));
-		_uc.printAnswer(ret, ret ? _labels.getString("S3_good")
-				: _labels.getString("S3_bad"));
-		return ret;
+		return output;
 	}
-
+	
 	@Override
 	public boolean isCritical()
 	{
