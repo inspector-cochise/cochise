accc71f1255b72515ebecf6e1f58a151622456e6
inspector-cochise <immanuel.sims@akquinet.de>
2012-05-31 11:01:50 +0200

    now checking for existence of java and proper version

--- TomcatMods/startCochise.sh
+++ TomcatMods/startCochise.sh
@@ -1,5 +1,22 @@
 #!/bin/sh
 
+#check if java is existent
+java > /dev/null 2>&1
+if [ $? != 0 ]
+then
+	echo "Seems like there is no jre (i.e. java command) in your path. Please put a java command (at least version 1.6) in your path."
+	exit 1
+fi
+
+JAVA_VERSION=`java -version 2>&1 | head -n 1`
+#check if java is at least version 6
+if [ 6 -gt `echo $JAVA_VERSION | grep -o "[[:digit:]]\.[[:digit:]]*" | grep -o "[[:digit:]]*$"` ]
+then
+	echo "Seems like your jre is to old for cochise. You have " $JAVA_VERSION " . You will need at least version 1.6 ."
+	exit 1
+fi
+
+
 KEYSTORE="./conf/.keystore"
 STOREPASS="changeit"
 KEYPASS="changeit"
@@ -28,7 +45,7 @@ do
 
 	if [ $PW1 != $PW2 ]
 	then
-		echo "Your passwords were not equal..."
+		echo "Your passwords have not been equal..."
 	fi
 done
 
