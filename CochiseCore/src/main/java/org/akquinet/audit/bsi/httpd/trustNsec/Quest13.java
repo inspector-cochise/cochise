f0d26906720237fa16f2fe0fee034d0a051b2f03
inspector-cochise <immanuel.sims@akquinet.de>
2012-08-17 17:28:12 +0200

    first version of Quest13 - not ready yet.

---
+++ CochiseCore/src/main/java/org/akquinet/audit/bsi/httpd/trustNsec/Quest13.java
@@ -0,0 +1,220 @@
+package org.akquinet.audit.bsi.httpd.trustNsec;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.ResourceBundle;
+import java.util.Set;
+
+import org.akquinet.audit.ModuleHelper;
+import org.akquinet.audit.YesNoQuestion;
+import org.akquinet.audit.bsi.httpd.PrologueData;
+import org.akquinet.audit.ui.UserCommunicator;
+import org.akquinet.httpd.ConfigFile;
+import org.akquinet.httpd.syntax.Directive;
+
+public class Quest13 implements YesNoQuestion
+{
+	private static final String SSLCipherSuite_Minimum = "SSLCipherSuite !NULL:!eNULL:!aNULL:!ADH:!MD5:!RC2";
+
+	private static final String _id = "Quest13";
+	
+	private transient UserCommunicator _uc;
+	private transient ResourceBundle _labels;
+	
+	private ConfigFile _config;
+	private ModuleHelper _moduleHelper;
+	
+	public Quest13(PrologueData pd)
+	{
+		this(pd, UserCommunicator.getDefault());
+	}
+	
+	public Quest13(PrologueData pd, UserCommunicator uc)
+	{
+		this(pd._conf, pd._apacheExecutable, uc);
+	}
+
+	public Quest13(ConfigFile conf, File apacheExecutable)
+	{
+		this(conf, apacheExecutable, UserCommunicator.getDefault());
+	}
+	
+	public Quest13(ConfigFile conf, File apacheExecutable, UserCommunicator uc)
+	{
+		_uc = uc;
+		_config = conf;
+		_labels = ResourceBundle.getBundle(_id, _uc.getLocale());
+		_moduleHelper = new ModuleHelper(_config, apacheExecutable);
+	}
+	
+	@Override
+	public boolean answer()
+	{
+		_uc.printHeading3( _labels.getString("name") );
+		_uc.printParagraph( _labels.getString("Q0") );
+		
+		if(!mod_sslIsPresent() ||
+		   !SSLRandomSeedIsWellAndPresent() ||
+		   !SSLMutexIsWellAndPresent() ||
+		   !SSLCipherSuiteIsWellAndPresent())
+		{
+			_uc.printAnswer(false, _labels.getString("A0"));
+			return false;
+		}
+		else
+		{
+			_uc.printAnswer(true, _labels.getString("A1"));
+			return true;
+		}
+	}
+
+	private boolean mod_sslIsPresent()
+	{
+		if(_moduleHelper.isModuleLoaded("ssl_module") || _moduleHelper.isModuleCompiledInto("mod_ssl.c"))
+		{
+			_uc.println( _labels.getString("L1") );
+			return true;
+		}
+		else
+		{
+			_uc.println( _labels.getString("L2") );
+			return false;
+		}
+	}
+
+	private boolean SSLCipherSuiteIsWellAndPresent()
+	{
+		List<Directive> dirs = _config.getAllDirectivesIgnoreCase("SSLCipherSuite");
+		
+		if(dirs.size() != 1)
+		{
+			_uc.printParagraph( _labels.getString("L3") );
+			_uc.printExample(SSLCipherSuite_Minimum);
+			return false;
+		}
+		
+		Set<String> spec = new HashSet<String>( Arrays.asList( dirs.get(0).getValue().split(":") ));
+		
+		//TODO isn't that for high security only?
+		boolean ok = spec.contains("!NULL") &&
+						spec.contains("!eNULL") &&
+						spec.contains("!aNULL") &&
+						spec.contains("!ADH") &&
+						spec.contains("!MD5") &&
+						spec.contains("!RC2");
+		
+		if(!ok)
+		{
+			_uc.printParagraph( _labels.getString("L4") );
+			_uc.printExample(SSLCipherSuite_Minimum);
+			return false;
+		}
+		else
+		{
+			_uc.println( _labels.getString("L5") );
+			return true;
+		}
+	}
+
+	private boolean SSLMutexIsWellAndPresent()
+	{
+		List<Directive> dirs = _config.getAllDirectivesIgnoreCase("SSLMutex");
+		
+		if(dirs.size() != 1)
+		{
+			_uc.println( _labels.getString("L6") );
+			return false;
+		}
+		else if(dirs.get(0).getValue().equalsIgnoreCase("none") || dirs.get(0).getValue().equalsIgnoreCase("no"))
+		{
+			_uc.println( _labels.getString("L7") );
+			return false;
+		}
+		else
+		{
+			_uc.println( _labels.getString("L8") );
+			return true;
+		}
+	}
+
+	private boolean SSLRandomSeedIsWellAndPresent()
+	{
+		//TODO what would "well" mean?
+		final int randomSeedCount = _config.getAllDirectivesIgnoreCase("SSLRandomSeed").size();
+		if(randomSeedCount <= 0)
+		{
+			//TODO message?
+			return false;
+		}
+		else
+		{
+			//TODO message?
+			return true;
+		}
+	}
+
+	@Override
+	public void initialize() throws Exception
+	{
+		synchronized(this)
+		{
+			_config.reparse();
+			_moduleHelper.reparse();
+		}
+	}
+
+	@Override
+	public boolean isCritical()
+	{
+		return false;
+	}
+
+	@Override
+	public String getID()
+	{
+		return _id;
+	}
+
+	@Override
+	public String getName()
+	{
+		return _labels.getString("name");
+	}
+
+	@Override
+	public int getNumber()
+	{
+		return 13;
+	}
+
+	@Override
+	public int getBlockNumber()
+	{
+		return 4;
+	}
+
+	@Override
+	public String[] getRequirements()
+	{
+		String[] ret = new String[1];
+		ret[0] = "Quest6";
+		return ret;
+	}
+
+	@Override
+	public void setUserCommunicator(UserCommunicator uc)
+	{
+		_uc = uc;
+	}
+	
+	private synchronized void readObject( java.io.ObjectInputStream s ) throws IOException, ClassNotFoundException
+	{
+		s.defaultReadObject();
+		_uc = UserCommunicator.getDefault();
+		_labels = ResourceBundle.getBundle(_id, _uc.getLocale());
+	}
+
+}
