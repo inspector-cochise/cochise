3d7c8903cd1bf442cb51c8d77805a71ea142c5a8
inspector-cochise <immanuel.sims@akquinet.de>
2012-08-24 17:27:10 +0200

    forgot to commit this, was buggy

--- TomcatMods/startCochise.sh
+++ TomcatMods/startCochise.sh
@@ -1,26 +1,9 @@
 #!/bin/sh
 
-#check if java is existent
-java > /dev/null 2>&1
-if [ $? != 0 ]
-then
-	echo "Seems like there is no jre (i.e. java command) in your path. Please put a java command (at least version 1.6) in your path."
-	exit 1
-fi
-
-JAVA_VERSION=`java -version 2>&1 | head -n 1`
-#check if java is at least version 6
-if [ 6 -gt `echo $JAVA_VERSION | grep -o "[[:digit:]]\.[[:digit:]]*" | grep -o "[[:digit:]]*$"` ]
-then
-	echo "Seems like your jre is to old for cochise. You have " $JAVA_VERSION " . You will need at least version 1.6 ."
-	exit 1
-fi
-
-
 KEYSTORE="./conf/.keystore"
 STOREPASS="changeit"
 KEYPASS="changeit"
-CERTPARAMS="CN=*, OU=Cochise, O=Unknown, L=Unknown, ST=Unknown, C=Unknown"
+CERTPARAMS="CN=*,OU=Cochise,O=Unknown,L=Unknown,ST=Unknown,C=Unknown"
 
 if [ ! -e $KEYSTORE ]
 then
@@ -45,11 +28,16 @@ do
 
 	if [ $PW1 != $PW2 ]
 	then
-		echo "Your passwords have not been equal..."
+		echo "Your passwords were not equal..."
 	fi
 done
 
-export JAVA_OPTS="-DcochisePass=$PW1"
+if [ $1 = "debug" ]
+then
+	export JAVA_OPTS="-DcochisePass=$PW1 -Xdebug -Xrunjdwp:transport=dt_socket,address=8000,server=y,suspend=y"
+else
+	export JAVA_OPTS="-DcochisePass=$PW1"
+fi
 
 cd bin
 ./startup.sh
